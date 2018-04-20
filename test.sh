ca51f7d40e6a0f8a748fdc1081af69406ac9194c
Stephane Maarek <simplesteph@users.noreply.github.com>
2018-04-20 14:19:41 +0530

    fixed test.sh

--- test.sh
+++ test.sh
@@ -4,8 +4,9 @@ set -ex
 docker-compose -f $1 up -d
 sleep 10
 # some tests on Kafka
-running=`docker-compose -f $1 ps | grep Up (healthy) | wc -l`
+running=`docker-compose -f $1 ps | grep Up | wc -l`
 if [ "$running" != "$2" ]; then
+   docker-compose -f $1 logs
    exit 1
 fi
 docker-compose -f $1 down
