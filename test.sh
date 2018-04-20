ba4e4674a77ed55e0c8adf25f423c66f962a9917
Stephane Maarek <simplesteph@users.noreply.github.com>
2018-04-20 14:50:54 +0530

    added some kafka tests

--- test.sh
+++ test.sh
@@ -1,22 +1,38 @@
 #!/bin/bash
 set -ex
 
+all_great(){
+    # for testing
+    running=`docker-compose -f $1 ps | grep Up | wc -l`
+    if [ "$running" != "$2" ]; then
+        # for logging
+        docker-compose -f $1 ps
+        # debug
+        docker-compose -f $1 logs
+        exit 1
+    fi
+}
+
+kafka_tests(){
+    kafka-topics --create --topic test_topic --replication-factor 1 --partitions 12
+    for x in {1..100}; do echo $x; sleep 2; done | kafka-console-producer --broker-list localhost:9092 --topic test_topic
+    rows=`kafkacat -C -b localhost:9092 -t test_topic -o beginning -e | wc -l `
+    if [ "$rows" != "100" ]; then
+        kafkacat -C -b localhost:9092 -t test_topic -o beginning -e
+        exit 1
+    fi
+}
+
+# creating stack...
 docker-compose -f $1 up -d
 sleep 10
-# for logging
+# logging
 docker-compose -f $1 ps
-
-# TODO: some tests on Kafka
-
-# for testing
-running=`docker-compose -f $1 ps | grep Up | wc -l`
-if [ "$running" != "$2" ]; then
-    # for logging
-    docker-compose -f $1 ps
-    # debug
-    docker-compose -f $1 logs
-    exit 1
-fi
+# tests
+all_great $1 $2
+kafka_tests
+all_great $1 $2
+# teardown
 docker-compose -f $1 down
 
 
