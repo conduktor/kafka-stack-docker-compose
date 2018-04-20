e1f41475e173236fd8ca223ca381f8e2d8025bd2
Stephane Maarek <simplesteph@users.noreply.github.com>
2018-04-20 14:07:31 +0530

    travis first pass

---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -ex
+
+docker-compose -f $1 up  
+sleep 10
+# some tests on Kafka
+running = `docker-compose -f $1 ps | grep Up | wc -l`
+if [ "$running" != "$2" ]; then
+   exit 1
+fi
+docker-compose -f $1 down
+
+
