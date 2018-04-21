9d5875b4bef995950cd2921782508f4119046168
Stephane Maarek <simplesteph@users.noreply.github.com>
2018-04-21 10:13:08 +0530

    don't fail

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-set -e
 
 all_great(){
     # for testing
+    echo "Verifying Process"
     running=`docker-compose -f $1 ps | grep Up | wc -l`
     if [ "$running" != "$2" ]; then
         # for logging
@@ -14,14 +14,18 @@ all_great(){
 }
 
 kafka_tests(){
+    echo "Testing Kafka"
     topic="test_topic"
-    sleep 2 && kafka-topics --create --topic $topic --replication-factor 1 --partitions 12 --zookeeper localhost:2181
+    replication_factor="1"
+    for i in 1 2 3 4 5; do kafka-topics --create --topic $topic --replication-factor $replication_factor --partitions 12 --zookeeper localhost:2181 && break || sleep 5; done
     for x in {1..100}; do echo $x; done | kafka-console-producer --broker-list localhost:9092 --topic $topic
     rows=`kafka-console-consumer --bootstrap-server localhost:9092 --topic $topic --from-beginning --timeout-ms 2000 | wc -l`
     # rows=`kafkacat -C -b localhost:9092 -t $topic -o beginning -e | wc -l `
     if [ "$rows" != "100" ]; then
         kafka-console-consumer --bootstrap-server localhost:9092 --topic test-topic --from-beginning --timeout-ms 2000 | wc -l
         exit 1
+    else;
+        echo "Kafka Test Success"
     fi
 }
 
@@ -35,6 +39,4 @@ all_great $1 $2
 kafka_tests
 all_great $1 $2
 # teardown
-docker-compose -f $1 down
-
-
+echo "Success!"
